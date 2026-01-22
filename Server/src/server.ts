618efa193583f823430a353acc89ae6fc96da881
Hetul Mistry <85955968+HetulMistry@users.noreply.github.com>
2026-01-22 11:40:37 +0530

    feat: add dotenv configuration and socket.io integration

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,19 +2,33 @@ import express from "express";
 import http from "http";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
 
 import type { Request, Response } from "express";
 
-const app = express();
-
-const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 
 const server = http.createServer(app);
+
+const ioSocket = new Server(server);
+
+ioSocket.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
